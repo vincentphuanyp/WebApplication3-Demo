bdc816ae8fb592aa5cad38f2d1f3f8f20f1efc4d
vincentp <vincentp@DCLDEMO>
2024-01-16 10:06:10 +0800

    Add project files.

---
+++ WebApplication3/Model/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication3.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+
+        public string CreditCard { get; set; }
+
+    }
+}
